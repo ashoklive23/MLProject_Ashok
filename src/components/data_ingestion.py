05465c951c40edd97e29574d7d424eac22ea04f3
Ashok <ashoklive23@gmail.com>
2026-01-11 11:03:24 +0530

    Added Dataingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,48 @@
+# Read CSV file and load data into pandas DataFrame
+# Train and test split the data
+# Save the split datasets to specified file paths
+
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
+        try:
+            df = pd.read_csv('notebook/data/stud.csv')
+            logging.info("Dataset read as pandas dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info("Raw data saved to artifacts folder")
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of data is completed")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            logging.error("Error occurred during data ingestion")
+            raise CustomException(e, sys)
