6e822320bab0a0991de15e8b05cb63f29be0618c
Ashok <ashoklive23@gmail.com>
2026-01-17 10:16:54 +0530

    Model Params

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+
 def save_object(file_path: str, obj: object) -> None:
     """Saves a Python object to a file using pickle serialization.
 
@@ -25,6 +26,34 @@ def save_object(file_path: str, obj: object) -> None:
 
         with open(file_path, 'wb') as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+#evaluate models function
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
 
     except Exception as e:
         raise CustomException(e, sys)
